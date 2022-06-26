304930d6b894ed8594b345aa92d28b28e7804378
hasira1111 <108216829+hasira1111@users.noreply.github.com>
2022-06-26 15:06:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94767926078']
+global.premium = ['94767926078']
+global.ownernomer = '94767926078'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
